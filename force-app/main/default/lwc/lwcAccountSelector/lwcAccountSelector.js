b20bee73594d3abf3e0880b235b6aeac9004652c
James H <james@sparkworks.io>
2019-01-26 13:38:00 -0800

    accountSelector conversion to LWC

---
+++ force-app/main/default/lwc/lwcAccountSelector/lwcAccountSelector.js
@@ -0,0 +1,33 @@
+import { LightningElement, track, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { fireEvent } from 'c/pubsub';
+import getAccountOptions from '@salesforce/apex/DataServiceCtrl.getAccountOptions';
+
+export default class LwcAccountSelector extends LightningElement {
+  @track topAccounts;
+
+  @wire(CurrentPageReference) pageRef;
+
+  @wire(getAccountOptions)
+  wiredTopAccounts({ error, data }) {
+    if (data) {
+      this.topAccounts = JSON.parse(data).items;
+    } else if (error) {
+      this.dispatchEvent(
+        new ShowToastEvent({
+          message: error,
+          variant: "error",
+        })
+      );
+    }
+  }
+
+  handleAccountOptionSelected(event) {
+    fireEvent(this.pageRef, 'account_id_seleted', event.target.value);
+  }
+
+  handleClearTableOnly() {
+    fireEvent(this.pageRef, 'header_clear_table');
+  }
+}

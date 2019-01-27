36cfed253a7c6da8ba877cd79462979080236e48
James H <james@sparkworks.io>
2019-01-26 21:49:30 -0800

    added tab to show @wire with reactive SOQL string

--- force-app/main/default/lwc/datatableExample/datatableExample.js
+++ force-app/main/default/lwc/datatableExample/datatableExample.js
@@ -1,21 +1,42 @@
 import { LightningElement, track } from 'lwc';
 
+const _defaultQueryString = 'SELECT Id, Name, UserName, Email FROM User';
 const _defaultConfig = {
-  queryString: 'SELECT Id, Name, UserName, Email FROM User'
+  queryString: _defaultQueryString
 }
+const DELAY = 2000;
 
 export default class DatatableExample extends LightningElement {
   @track data;
   @track columns;
   @track tableRequest;
   @track tableResponse;
+  @track query;
 
   connectedCallback() {
+    this.query = _defaultQueryString;
     this.tableRequest = _defaultConfig;
+  }
+
+  handleKeyChange(event) {
+    window.clearTimeout(this.delayTimeout);
+    this.query = event.target.value;
+    this.delayTimeout = setTimeout(() => {
+      this.tableRequest = {
+        queryString: this.query
+      };
+    }, DELAY);
   }
   
   handleSuccess(event) {
     this.data = event.detail.tableData;
     this.columns = event.detail.tableColumns;
   }
+
+  handleError(event) {
+    console.log(event);
+    this.data = null;
+    this.columns = null;
+  }
+  
 }
--- force-app/main/default/lwc/lwcDatatableService/lwcDatatableService.js
+++ force-app/main/default/lwc/lwcDatatableService/lwcDatatableService.js
@@ -1,10 +1,21 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import wireTableCache from '@salesforce/apex/DataTableService.wireTableCache';
 
 export default class LwcDatatableService extends LightningElement {
-  @api requestConfig;
+  @api 
+  get requestConfig() {
+    return this.request;
+  }
+  set requestConfig(value) {
+    // @wire can't tell a difference in change if the object isn't stringified?
+    this.request = JSON.stringify(value);
+    // For example the below wouldn't trigger a real change, we are stuck with a cached response
+    //this.request = value
+  }
+  
+  @track request;
 
-  @wire(wireTableCache, { tableRequest: '$requestConfig' })
+  @wire(wireTableCache, { tableRequest: '$request' })
   wiredCache({ error, data }) {
     if (data) {
       let response = {
@@ -15,7 +26,9 @@ export default class LwcDatatableService extends LightningElement {
         new CustomEvent('success', {detail: response})
       );
     } else if (error) {
-      console.log(error);
+      this.dispatchEvent(
+        new CustomEvent('error', {detail: error})
+      );
     }
   }
   

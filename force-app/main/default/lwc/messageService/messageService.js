52549bae2e331864e8b41a40d98dfc021e7fb8b9
James Hou <james@sparkworks.io>
2020-06-19 23:54:51 -0700

    Add messageService (lwc) and MessageServiceHandler (aura)

---
+++ force-app/main/default/lwc/messageService/messageService.js
@@ -0,0 +1,81 @@
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, james@sparkworks.io
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * - Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import { LightningElement, api, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { publish, MessageContext } from 'lightning/messageService';
+import OPEN_CHANNEL from '@salesforce/messageChannel/OpenChannel__c';
+
+// Toast
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { reduceErrors } from 'c/utils';
+
+export default class MessageService extends LightningElement {
+    @api scopedId;
+    @wire(CurrentPageReference) pageRef;
+    @wire(MessageContext) messageContext;
+
+    /* LWC broker to Aura */
+    @api
+    dialogService(payload) {
+        const boundary = { scopedId: this.scopedId };
+        publish(this.messageContext, OPEN_CHANNEL, { key: 'dialogService', value: { ...payload, ...boundary } });
+    }
+
+    @api
+    notifyClose() {
+        publish(this.messageContext, OPEN_CHANNEL, { key: 'notifyClose' });
+    }
+
+    @api
+    notifySuccess(title, message = null) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: title,
+                message: message,
+                variant: 'success'
+            })
+        );
+    }
+
+    @api
+    notifySingleError(title, error) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: title,
+                message: reduceErrors(error)[0],
+                variant: 'error',
+                mode: 'sticky'
+            })
+        );
+    }
+}
